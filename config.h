886ab357369548d411dea76d31a97104c93443cb
hariharan <97215411+hari06021999@users.noreply.github.com>
2023-08-03 17:19:03 +0530

    Update config.h

--- config.h
+++ config.h
@@ -25,16 +25,19 @@ const uint8_t R_PWM = 9;//D3
 const uint8_t L_PWM = 3;//D9
 const uint8_t right_relay = 2;// D2 RIGHT MOTOR
 const uint8_t left_relay = 4;// D4 RIGHT  MOTOR
-const uint8_t limit_switch = 5;
+const uint8_t limit_switch = 6;
 
 /* Left pwm and right pwm variable to access global */
 uint16_t lPwm;
 uint16_t rPwm;
+
+long sig_started=0;
+long pwm_interval=50;
 /* Industrial pc publish linear and angular value store in below variable to access through out program*/
 float linear_velocity_ref;
 float angular_velocity_ref;
 
 uint8_t speed_right=25,speed_left=25; // speed increase variable
 uint8_t limit_flag=0;//limit switch flag
+uint8_t limit_sw_State;
 typedef enum {ZERO,ONE,TWO,THREE,FOUR,FIVE}number;
-
