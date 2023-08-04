6296cfd72450ac6f77ac9093395c11f1b9e15499
hariharan <97215411+hari06021999@users.noreply.github.com>
2023-08-04 23:42:15 +0800

    Update nano_node.ino

--- nano_node.ino
+++ nano_node.ino
@@ -9,11 +9,15 @@
 #include <SoftwareSerial.h> // Debug serial
 SoftwareSerial mySerial(6, 7); // RX, TX
 void onTwist(const geometry_msgs::Twist &msg);
+void low_pwm(const std_msgs::Bool &msg);
+void medium_pwm(const std_msgs::Bool &msg);
+void high_pwm(const std_msgs::Bool &msg);
 /* Initialize node handle for ros communication */ 
 ros::NodeHandle node;
 ros::Subscriber<geometry_msgs::Twist> sub("/linear/angular", &onTwist);// command velocity subscribe topic creation
- 
-
+ros::Subscriber<std_msgs::Bool> pwm_1("/pwm/low", &low_pwm);// low pwm subscribe topic creation
+ros::Subscriber<std_msgs::Bool> pwm_2("/pwm/medium", &medium_pwm);// medium pwm subscribe topic creation
+ros::Subscriber<std_msgs::Bool> pwm_3("/pwm/high", &high_pwm);// high pwm subscribe topic creation
 void setup() {
   setupPins(); // gpio pins initialize
    mySerial.begin(9600);// baudrate for debug serial communcation
@@ -24,7 +28,9 @@ void setup() {
   node.getHardware()->setBaud(115200);
   node.initNode(); // ros node intialize
   node.subscribe(sub); //subcribe joystick value
-
+  node.subscribe(pwm_1); //subcribe pwm low value
+ node.subscribe(pwm_2); //subcribe pwm medium value
+ node.subscribe(pwm_3); //subcribe pwm high value
 }
 
 void loop() {
@@ -102,18 +108,49 @@ void onTwist(const geometry_msgs::Twist &msg)
 //  mySerial.println(msg.linear.x);
 
 }
-
-   
+void low_pwm(const std_msgs::Bool &msg)
+{
+    int pwm_value = msg.data;
+   if(pwm_value == 1)
+   {
+      PWMRANGE = 70;
+   }
+   if(pwm_value == 0)
+   {
+      PWMRANGE = 0;
+   }
+}
+void medium_pwm(const std_msgs::Bool &msg)
+{
+   int pwm_value = msg.data;
+   if(pwm_value == 1)
+   {
+      PWMRANGE = 90;
+   }
+   if(pwm_value == 0)
+   {
+      PWMRANGE = 0;
+   }
+}
+void high_pwm(const std_msgs::Bool &msg)
+{
+    int pwm_value = msg.data;
+   if(pwm_value == 1)
+   {
+      PWMRANGE = 120;
+   }
+   if(pwm_value == 0)
+   {
+      PWMRANGE = 0;
+   }
+}
    
    
 /* Set all gpio pins */
 void setupPins()
 {
-   
   pinMode(right_relay, OUTPUT);
   pinMode(left_relay, OUTPUT);
-  pinMode(limit_switch, INPUT_PULLUP);
-  
 }
 
 // Map x value from [0 .. 1] to [out_min .. out_max]
